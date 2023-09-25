a13d62c8e6f9f0e28872ed0d7ad6f858d3c8bfa5
nefren <woytekk12@gmail.com>
2023-09-25 15:47:59 +0200

    layouts

--- public/static/front/js/scripts.js
+++ public/static/front/js/scripts.js
@@ -1,3 +1,4 @@
+//slider
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
 const slideWidth = slides[0].clientWidth;
@@ -17,11 +18,22 @@ function updateSlider() {
     slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
 
-setInterval(nextSlide, 5000); // Automatic scrolling every 5 seconds
+setInterval(nextSlide, 5000);
 
-// Add event listeners for arrow buttons
 const arrowLeft = document.querySelector('.arrow-left');
 const arrowRight = document.querySelector('.arrow-right');
 
 arrowLeft.addEventListener('click', prevSlide);
 arrowRight.addEventListener('click', nextSlide);
+
+/*top headers*/
+const postList = document.querySelectorAll(".article-full");
+postList.forEach(item => {
+    if (item.classList.contains('schema-1') || item.classList.contains('schema-2')) {
+        const headers = item.querySelector(".article-full__headers");
+        item.prepend(headers);
+    }
+});
+
+/*moving the main photo to the beginning */
+$(".article-full__content .article-full__image-img").prependTo(".article-full__image");
